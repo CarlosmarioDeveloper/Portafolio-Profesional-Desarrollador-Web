7ba413c80ca2fa5c65ee37c0b143904aa25e26c4
CarlosmarioDeveloper <101945673+CarlosmarioDeveloper@users.noreply.github.com>
2022-04-02 11:17:43 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,122 @@
+
+  window.sr = ScrollReveal();
+  sr.reveal('.container_img', {
+    duration: 1200,
+    origin: 'top',
+    distance: '200px',
+   
+  });
+
+   window.sr = ScrollReveal();
+  sr.reveal('.container_linea', {
+    duration: 1000,
+    origin: 'left',
+    distance: '200px',
+    delay:400
+  });
+
+   window.sr = ScrollReveal();
+  sr.reveal('.container_description_info_developer', {
+    duration: 1000,
+    origin: 'left',
+    distance: '200px',
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.container_info_sobre_mi', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '200px',
+    delay:980
+    
+  });
+
+   window.sr = ScrollReveal();
+  sr.reveal('.container_img_svg', {
+    duration: 1000,
+    origin: 'left',
+    distance: '200px',
+    
+  });
+
+   window.sr = ScrollReveal();
+  sr.reveal('.container_skills_title', {
+    duration: 900,
+    origin: 'top',
+    distance: '100px',
+  });
+
+   window.sr = ScrollReveal();
+  sr.reveal('.card1', {
+    duration: 850,
+    origin: 'bottom',
+    distance: '100px',
+    
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.card2', {
+    duration: 900,
+    origin: 'bottom',
+    distance: '100px',
+    delay:200
+    
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.card3', {
+    duration: 900,
+    origin: 'bottom',
+    distance: '100px',
+    delay:400
+    
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.card4', {
+    duration: 900,
+    origin: 'bottom',
+    distance: '100px',
+    delay:600
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.container_main_proyecto', {
+    duration: 1000,
+    origin: 'left',
+    distance: '600px',
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.container_cards_proyecto', {
+    duration: 900,
+    origin: 'bottom',
+    distance: '400px',
+    delay:600
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.card-shadow', {
+    duration: 900,
+    origin: 'bottom',
+    distance: '400px'
+  });
+
+   window.sr = ScrollReveal();
+  sr.reveal('.cards-proyecto', {
+    duration: 1100,
+    origin: 'bottom',
+    distance: '10px',
+    delay:1200
+  });
+
+
+
+
+
+
+
+
+
+
+
